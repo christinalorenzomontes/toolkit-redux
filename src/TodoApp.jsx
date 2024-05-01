681d3370cb92f5db55e7a6f1931d8fa4bea9b9b5
Christina Lorenzo <christinalorenzo@gmail.com>
2024-05-01 10:06:44 +0200

    RTK Query

---
+++ src/TodoApp.jsx
@@ -0,0 +1,16 @@
+
+export const TodoApp = () => {
+  return (
+    <>
+      <h1>Todos - RTK Query</h1>
+      <hr />
+      <h2>isLoading...</h2>
+      
+      <pre>...</pre>
+
+      <button>
+        Next Todo
+      </button>
+    </>
+  )
+}
