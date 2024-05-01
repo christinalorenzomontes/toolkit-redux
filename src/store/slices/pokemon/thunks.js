bf0e1d709bf8f079994dfcd174b5837102473087
Christina Lorenzo <christinalorenzo@gmail.com>
2024-05-01 09:20:56 +0200

    install axios

--- src/store/slices/pokemon/thunks.js
+++ src/store/slices/pokemon/thunks.js
@@ -1,3 +1,4 @@
+import { pokemonApi } from "../../../api/pokemonApi";
 import { setPokemons, startLoadingPokemons } from "./pokemonSlice"
 
 export const getPokemons = ( page = 0 ) => {
@@ -5,10 +6,11 @@ export const getPokemons = ( page = 0 ) => {
   return async( dispatch, getState ) => {
     dispatch( startLoadingPokemons() );
 
-    // TODO Realizar petición HTTP
-    const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${ page * 10 }`);
-    const data = await resp.json();
-    console.log(data);
+    // // TODO Realizar petición HTTP
+    // const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${ page * 10 }`);
+    // const data = await resp.json();
+    const { data } = await pokemonApi.get(`/pokemon?limit=10&offset=${ page * 10 }`);
+    // console.log(resp);
 
     dispatch( setPokemons({ pokemons: data.results, page: page +1 }) );
   }
